2c37f5efa33e4efc4c2e3646f8bd7489d2406bc4
kkangwook <send2woohyun@naver.com>
2025-05-27 16:55:44 +0900

    Update web_crawl.py

--- mid_project/web_crawl.py
+++ mid_project/web_crawl.py
@@ -76,3 +76,69 @@ for i in query:
 
 driver.quit()
 
+
+
+#mwlon
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import os
+import time
+
+from selenium.webdriver.chrome.options import Options
+# 로봇 무시 
+options = Options()
+options.add_argument('--disable-blink-features=AutomationControlled')
+query=['에스파 아마겟돈']
+# 크롬 driver 생성 
+driver_path = ChromeDriverManager().install()
+correct_driver_path = os.path.join(os.path.dirname(driver_path), "chromedriver.exe")
+driver = webdriver.Chrome(service=Service(executable_path=correct_driver_path), options=options)
+url='https://www.melon.com/index.htm'
+driver.get(url)
+time.sleep(3)
+box=driver.find_element(By.ID,'top_search')
+box.send_keys(query)
+box.send_keys(Keys.ENTER)
+time.sleep(2)
+supernova=driver.find_element(By.ID,'frm_songList')
+memo=supernova.find_element(By.CLASS_NAME,'btn_icon_detail')
+memo.click()
+time.sleep(2)
+info=[]
+name=driver.find_element(By.CLASS_NAME, 'song_name')
+info.append(name.text)
+name2=driver.find_element(By.XPATH, '/html/body/div[1]/div[3]/div/div/div/form/div/div/div[2]/div[1]/div[2]/a/span[1]')
+info.append(name2.text)
+name3=driver.find_element(By.XPATH, '/html/body/div[1]/div[3]/div/div/div/form/div/div/div[2]/div[2]/dl/dd[3]')
+info.append(name3.text)
+name4=driver.find_element(By.ID, 'd_video_summary')
+info.append(name4.text.replace('\n',' '))
+name5=driver.find_element(By.XPATH, '/html/body/div[1]/div[3]/div/div/div/form/div/div/div[2]/div[1]/div[2]/a/span[1]')
+name5.click()
+time.sleep(2)
+name6=driver.find_element(By.ID, 'd_like_count')
+info.append(name6.text)
+
+print(info)
+driver.quit()
+
+
+
+
+/html/body/div/div[3]/div/div/div[4]/div/div[2]/div[1]/form/table/tbody/tr[6]/td[4]/div/div/div[1]/span/strong/a
+/html/body/div/div[3]/div/div/div[4]/div/div[2]/div[1]/form/table/tbody/tr[7]/td[4]/div/div/div[1]/span/strong/a
+
+/html/body/div/div[3]/div/div/div[4]/div/div[2]/div[1]/form/table/tbody/tr[96]/td[4]/div/div/div[1]/span/strong/a
+/html/body/div/div[3]/div/div/div[4]/div/div[2]/div[1]/form/table/tbody/tr[96]/td[4]/div/div/div[2]/div[1]/a
+url='https://www.melon.com/chart/age/index.htm?chartType=YE&chartGenre=KPOP&chartDate='+'2024'
+data=[]
+for i in range(1,101):
+	a=f'/html/body/div/div[3]/div/div/div[4]/div/div[2]/div[1]/form/table/tbody/tr[{i}]/td[4]/div/div/div[1]/span/strong/a'
+	song=driver.get(By.XPATH, a)
+	data.append(song.text)
+
